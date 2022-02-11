a6a89975eb67de2df8d39722c06daaa6226a62e9
L0laapk3 <L0laapk3@gmail.com>
2022-02-11 21:07:41 +0100

    added information about the past

--- src/shared/SharedObjects.h
+++ src/shared/SharedObjects.h
@@ -38,17 +38,27 @@ struct Car : PhysicsObj {
 	unsigned char team;
 	Vec3 hitBox;
 	Vec3 hitBoxOffset;
-	struct {
-		unsigned int frontLeftWheelContact : 1;
-		unsigned int frontRightWheelContact : 1;
-		unsigned int backLeftWheelContact : 1;
-		unsigned int backRightWheelContact : 1;
-		unsigned int onGround : 1;
-		unsigned int hasFlip : 1;
-		unsigned int superSonic : 1;
-		unsigned int demolished : 1;
-		unsigned int bot : 1;
-		unsigned int RLBotBMControlled : 1;
+
+	int demolishedAt = std::numeric_limits<int>::min();
+	int flippedAt = std::numeric_limits<int>::min();
+	union {
+		struct {
+			unsigned int frontLeftWheelContact : 1;
+			unsigned int frontRightWheelContact : 1;
+			unsigned int backLeftWheelContact : 1;
+			unsigned int backRightWheelContact : 1;
+			unsigned int frontLeftWheelReset : 1;
+			unsigned int frontRightWheelReset : 1;
+			unsigned int backLeftWheelReset : 1;
+			unsigned int backRightWheelReset : 1;
+			unsigned int onGround : 1;
+			unsigned int hasFlip : 1;
+			unsigned int superSonic : 1;
+			unsigned int demolished : 1;
+			unsigned int bot : 1;
+			unsigned int RLBotBMControlled : 1;
+		};
+		unsigned int flags;
 	};
 };
 
@@ -59,7 +69,7 @@ struct SharedMemoryObj {
 	std::array<Ball, 8> balls;
 	unsigned int numBalls;
 
-	std::bitset<256> boostPads;
+	std::array<int, 128> boostPads;
 	unsigned int numBoostPads;
 
 	int tick;
