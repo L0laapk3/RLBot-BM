f5e58025f6dc4ab186900e08ce1db12b519e5395
L0laapk3 <L0laapk3@gmail.com>
2022-10-29 19:42:33 +0200

    add dllexport and appveyor release build

--- src/RLBotBM_c.h
+++ src/RLBotBM_c.h
@@ -11,20 +11,21 @@
 
 struct RLBotBM_H;
 // can return null pointers if it fails to connect to the shared memory
-RLBotBM_H* RLBotBM_create();
-void RLBotBM_destroy(RLBotBM_H* handle);
+__declspec(dllexport) RLBotBM_H* RLBotBM_create();
 
-void RLBotBM_getCurrentState(RLBotBM_H* handle, GameStateObj* state);
+__declspec(dllexport) void RLBotBM_destroy(RLBotBM_H* handle);
+
+__declspec(dllexport) void RLBotBM_getCurrentState(RLBotBM_H* handle, GameStateObj* state);
 
 // set controls for bot at carIndex
-void RLBotBM_setBotInput(RLBotBM_H* handle, const ControllerInput* controls, const int carIndex);
+__declspec(dllexport) void RLBotBM_setBotInput(RLBotBM_H* handle, const ControllerInput* controls, const int carIndex);
 
 // test if the next tick is available (state.tick) and if so, update state
-bool RLBotBM_pollNextTick(RLBotBM_H* handle, GameStateObj* state);
+__declspec(dllexport) bool RLBotBM_pollNextTick(RLBotBM_H* handle, GameStateObj* state);
 // wait until next tick is available (state.tick) and update state, return true if the thread had to wait
-bool RLBotBM_waitNextTick(RLBotBM_H* handle, GameStateObj* state);
+__declspec(dllexport) bool RLBotBM_waitNextTick(RLBotBM_H* handle, GameStateObj* state);
 
-StateSetObj* RLBotBM_getStateSetObj(RLBotBM_H* handle);
+__declspec(dllexport) StateSetObj* RLBotBM_getStateSetObj(RLBotBM_H* handle);
 
 
 #ifdef __cplusplus
