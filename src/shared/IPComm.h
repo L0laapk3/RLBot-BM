62e4b25b667cb01734ba6f78e9b52e4c80dae21c
L0laapk3 <L0laapk3@gmail.com>
2022-10-29 17:25:55 +0200

    change stdexception to stdruntimeerror

--- src/shared/IPComm.h
+++ src/shared/IPComm.h
@@ -10,21 +10,21 @@ namespace RLBotBM::Shared {
 
 class IPComm {
 public:
-	struct Exception : public std::exception {
+	struct IPCommException : public std::runtime_error {
 		static std::string getLastErrorMessage() {
 			DWORD errorCode = GetLastError();
 			char errorMessage[1024];
 			FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, errorCode, 0, errorMessage, sizeof(errorMessage), NULL);
 			return std::string(errorMessage);
 		}
-		Exception(std::string desc) : std::exception((desc + ": " + getLastErrorMessage()).c_str()) {}
+		IPCommException(std::string desc) : std::runtime_error((desc + ": " + getLastErrorMessage()).c_str()) {}
 	};
-	struct CreateFileMappingException : public Exception { CreateFileMappingException() : Exception("Failed to create file mapping object") {} };
-	struct OpenFileMappingException : public Exception { OpenFileMappingException() : Exception("Failed to open file mapping object") {} };
-	struct MapViewOfFileException : public Exception { MapViewOfFileException() : Exception("Failed to open map view of file") {} };
-	struct CreateSemaphoreException : public Exception { CreateSemaphoreException() : Exception("Failed to create semaphore") {} };
-	struct SharedMemoryExistsException : public Exception { SharedMemoryExistsException() : Exception("Shared memory already exists") {} };
-	struct SharedMemoryDoesNotExistException : public Exception { SharedMemoryDoesNotExistException() : Exception("Shared memory does not exist") {} };
+	struct CreateFileMappingException : public IPCommException { CreateFileMappingException() : IPCommException("Failed to create file mapping object") {} };
+	struct OpenFileMappingException : public IPCommException { OpenFileMappingException() : IPCommException("Failed to open file mapping object") {} };
+	struct MapViewOfFileException : public IPCommException { MapViewOfFileException() : IPCommException("Failed to open map view of file") {} };
+	struct CreateSemaphoreException : public IPCommException { CreateSemaphoreException() : IPCommException("Failed to create semaphore") {} };
+	struct SharedMemoryExistsException : public IPCommException { SharedMemoryExistsException() : IPCommException("Shared memory already exists") {} };
+	struct SharedMemoryDoesNotExistException : public IPCommException { SharedMemoryDoesNotExistException() : IPCommException("Shared memory does not exist") {} };
 
 
 	IPComm(bool create);
